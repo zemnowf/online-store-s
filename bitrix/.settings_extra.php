2678f864a24606972f59b80d30f29e6e1baee9a2
Daniil Zemnov <zemnowf@gmail.com>
2023-05-15 01:12:42 +0300

    metatags smart filter

---
+++ bitrix/.settings_extra.php
@@ -0,0 +1,7 @@
+<?
+return array(
+    'smart_filter_template' => array(
+        'value' => '{h1} . {section.name}. {smartfilter.params.name} - {smartfilter.params.value} Цена - {smartfilter.price}',
+        'readonly' => true,
+    )
+);
